7e1690c531516b203620db649cc8ad6d5bd248a5
Christian Martinez <christian@mixdlabs.com>
2023-07-09 10:17:07 -0600

    Added schema, moved connection file to db folder. Hopefully that works

--- db/connection.js
+++ db/connection.js
@@ -7,7 +7,7 @@ const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: process.env.DB_PASSWORD,
-  database: 'test',
+  database: 'employees',
 })
 
 module.exports = connection
