69d4f47ed292aa137f5201f774c89ff222fab4da
Christian Martinez <christian@mixdlabs.com>
2023-07-10 10:09:38 -0600

    Fixed a bug where the addEmployees manager was not populating in the database, previously null now it shows the managers name. It is answer.allManagers not answer.managers.

--- index.js
+++ index.js
@@ -126,7 +126,7 @@ const addEmployee = () => {
                       },
                     ])
                     .then((answer) => {
-                      const manager = answer.manager
+                      const manager = answer.allManagers
                       fullName.push(manager)
                       const query = `INSERT INTO employee (first_name, last_name, role_id, manager_id)
                                     VALUES (?, ?, ?, ?)`
